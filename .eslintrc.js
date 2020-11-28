35b49f7243b72db42d3768b43ac97572d04d753d
Bilal <bhaidar@gmail.com>
2020-11-29 01:15:50 +0200

    Make .eslint a js file

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "plugin:prettier/recommended",
+    "eslint:recommended"
+  ],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  }
+}
